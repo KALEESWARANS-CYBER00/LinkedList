a878812a7f099d0d0ec7a0d636d61f5aa247354c
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-07-17 19:12:01 +0530

    Test: Implemented and tested insert-if-not-found logic

--- LinkedList.java
+++ LinkedList.java
@@ -1,12 +1,12 @@
-import SingleLinkedList.Insertion.SingleInsertReverseOrder;
+import SingleLinkedList.Insertion.SingleInsertIfNotFoundAtEnd;
 import SingleLinkedList.Traversal.PrintListForward;
 
 public class LinkedList {
     public static void main(String[] args) {
-        SingleInsertReverseOrder list = new SingleInsertReverseOrder();
+        SingleInsertIfNotFoundAtEnd list = new SingleInsertIfNotFoundAtEnd();
 
-        int[] values = {10, 20, 30, 40, 50};
-        list.insertMultipleReverse(values); // Inserts in reverse
+        int[] input = {10, 20, 30, 20, 40, 10}; // Duplicates will be ignored
+        list.insertMultipleIfNotFound(input);
 
         PrintListForward printer = new PrintListForward();
         printer.printListForward(list.getHead());
