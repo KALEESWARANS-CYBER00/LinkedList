a1c064f574b2926ec1f1ced2d265b490da0dfb74
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-07-17 19:00:54 +0530

    Update: Test added for recursive insertion in singly linked list

--- LinkedList.java
+++ LinkedList.java
@@ -3,15 +3,11 @@ import SingleLinkedList.Traversal.*;
 
 public class LinkedList {
     public static void main(String[] args) {
-        SingleInsertAfterValue list = new SingleInsertAfterValue();
+        SingleInsertRecursive list = new SingleInsertRecursive();
 
-        list.insertAtEnd(10);
-        list.insertAtEnd(20);
-        list.insertAtEnd(30);
-
-        list.insertAfterValue(20, 25);  // Inserts 25 after 20
-        list.insertAfterValue(30, 35);  // Inserts 35 after 30
-        list.insertAfterValue(99, 50);  // Value not in list
+        list.insertAtEndRecursive(10);
+        list.insertAtEndRecursive(20);
+        list.insertAtEndRecursive(30);
 
         PrintListForward printer = new PrintListForward();
         printer.printListForward(list.getHead());
